7df95989300577e83288b548bf1e7de44524a1dc
he.wenyao <he.wenyao@qq.com>
2023-09-08 00:04:09 +0800

    first commit

---
+++ src/main/java/com/zeroonedance/javaadminserver/JavaAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.zeroonedance.javaadminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAdminServerApplication.class, args);
+    }
+
+}
