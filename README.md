c338099d51c6cbc02d30898774de874b70d50ac9
he.wenyao <he.wenyao@nint.com>
2023-09-08 18:50:41 +0800

    add user model

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# java-admin-api
+## java-admin-api
+
+
+### 环境搭建
+#### Docker 安装 postgres
+拉取镜像
+```shell
+docker pull postgres
+```
+运行容器
+
+```shell
+docker run --name postgres-server -e POSTGRES_PASSWORD=P@ssw0rd -d -p 5432:5432 postgres
+```
---
+++ src/main/java/com/zeroonedance/adminapi/model/User.java
@@ -0,0 +1,23 @@
+package com.zeroonedance.adminapi.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+    @Id
+    private Long id;
+
+}
